93b7c54a313dd03c56f1bafa08910e58bd0ffcb6
Krithik Adav <112746235+adav-07@users.noreply.github.com>
2024-01-10 11:55:52 +0530

    Update ObjectOriented.java

--- ObjectOriented.java
+++ ObjectOriented.java
@@ -2,30 +2,41 @@ import java.util.stream.Stream;
 import java.util.function.Predicate;
 
 class FilterMethods{
+    
     public String[] filterItems(String[] items, Predicate<String> criteria) {
       return Stream.of(items).filter(criteria)
               .toArray(String[]::new);
     }
+    
     public Predicate<String> checkStringStartWithAny(String startString){
         Predicate<String> preciateFunctionObj =str -> str.startsWith(startString);
         return preciateFunctionObj;
     }
 }
 
-class Display{
-    public void display(String [] itemsStartsWith){
-        for(String item: itemsStartsWith){
+class ConsoleDisplayController{
+    
+    private String[] content;
+    
+    public void setContent(String [] itemsStartsWith){
+        content = itemsStartsWith;
+    }
+    
+    public void display(){
+        for(String item : content){
             System.out.println(item);
         }
     }
 }
 
-class HelloWorld {
+class Main {
+    
     public static void main(String[] args) {
         String[] items = {"hello","HI","WelComE","to","ZEISS"};
         FilterMethods Filterobject=new FilterMethods();
         String[] itemsStartsWith = Filterobject.filterItems(items, str -> str.startsWith("Z"));
-        Display displayObject=new Display();
-        displayObject.display(itemsStartsWith);
+        ConsoleDisplayController displayObject=new ConsoleDisplayController();
+        displayObject.setContent(itemsStartsWith);
+        displayObject.display();
     }
 }
