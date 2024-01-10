209b1920847e080f8a7a481671e9abb27ac1433f
Krithik Adav <112746235+adav-07@users.noreply.github.com>
2024-01-10 10:48:58 +0530

    Create ObjectOriented.java

---
+++ ObjectOriented.java
@@ -0,0 +1,27 @@
+import java.util.stream.Stream;
+import java.util.function.Predicate;
+
+class StringMethods{
+    public String[] filterItems(String[] items, Predicate<String> criteria) {
+      return Stream.of(items).filter(criteria)
+              .toArray(String[]::new);
+    }
+    public Predicate<String> checkStringStartWithAny(String startString){
+        Predicate<String> preciateFunctionObj =str -> str.startsWith(startString);
+        return preciateFunctionObj;
+    }
+    public void display(String [] itemsStartsWith){
+        for(String item: itemsStartsWith){
+            System.out.println(item);
+        }
+    }
+}
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String[] items = {"hello","HI","WelComE","to","ZEISS"};
+        StringMethods Stringobject=new StringMethods();
+        String[] itemsStartsWith = Stringobject.filterItems(items, str -> str.startsWith("H"));
+        Stringobject.display(itemsStartsWith);
+    }
+}
